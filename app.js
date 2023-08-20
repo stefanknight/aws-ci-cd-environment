2f7b9619b0ed118e021acaadb2a0dbbf3fb36530
Stefan G.P. Knight <sknight@spinvfx.com>
2023-08-20 14:26:53 -0400

    Pipeline project complete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('stefans repo'));
+app.get('/', (req, res) => res.send('AWS CI/CD project complete'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
